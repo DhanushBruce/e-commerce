b4dfac1691d649fc3f14e86b0cdfa5075f0d8199
Dhanush M <dhanush.m@thepenindia.com>
2025-04-15 03:59:30 -0700

    “Modified”

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Sample() {
-  return (
-    <div>Sample</div>
-  )
+  return <div>Sample</div>;
 }
 
-export default Sample
+export default Sample;
